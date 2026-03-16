ae373c60506f1665c8787ade6d0b5b079da1ab28
Shyam GK <100010392+Shyam-GK@users.noreply.github.com>
2026-03-16 15:33:52 +0530

    Modify Vite server configuration
    Updated Vite configuration to expose server on all network interfaces and set port to 4000.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',  // Expose to all network interfaces
+    port: 4000,       // You can change this to any port you prefer
+  },
   plugins: [react()],
-})
+});
