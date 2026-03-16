88e0d3a5e749647abc5e81d7e359293fd75fb172
Shyam GK <100010392+Shyam-GK@users.noreply.github.com>
2026-03-16 16:09:50 +0530

    Refactor vite.config.js for cleaner imports

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   server: {
-    host: '0.0.0.0',  // Expose to all network interfaces
-    port: 4000,       // You can change this to any port you prefer
+    host: '0.0.0.0',
+    port: 4000,
+    allowedHosts: 'all'
   },
-  plugins: [react()],
-});
+  plugins: [react()]
+})
